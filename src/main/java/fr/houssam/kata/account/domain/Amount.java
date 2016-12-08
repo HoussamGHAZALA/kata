a240a756460d9383307cb762f6d0c6e8dc43cbb5
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-08 17:39:13 +0100

    Validate amount value.

--- src/main/java/fr/houssam/kata/account/domain/Amount.java
+++ src/main/java/fr/houssam/kata/account/domain/Amount.java
@@ -10,4 +10,15 @@ import lombok.Value;
 @Builder
 public class Amount {
     private long value;
+
+    public Amount(long value) {
+        this.value = validate(value);
+    }
+
+    private long validate(long value) {
+        if(value > 0) {
+            return value;
+        }
+        throw new IllegalArgumentException("Amount should be positif");
+    }
 }
