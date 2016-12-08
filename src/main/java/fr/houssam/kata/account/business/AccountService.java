db4f8e30e2b2389f13bf4d09183191dc112986cf
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-08 16:51:24 +0100

    User can make deposit ih his account.

--- src/main/java/fr/houssam/kata/account/business/AccountService.java
+++ src/main/java/fr/houssam/kata/account/business/AccountService.java
@@ -1,5 +1,8 @@
 package fr.houssam.kata.account.business;
 
+import fr.houssam.kata.account.domain.Account;
+import fr.houssam.kata.account.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,8 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountService {
 
+    @Autowired
+    private AccountRepository accountRepository;
 
     public void depose(long deposit, String accountNumero) {
+        Account byNumero = accountRepository.findByNumero(accountNumero);
+        Long newSolde = byNumero.getSolde() + deposit;
 
+        Account accountToUpdate = Account.builder().id(byNumero.getId())
+                .solde(newSolde)
+                .numero(accountNumero)
+                .customer(byNumero.getCustomer()).build();
+
+        accountRepository.save(accountToUpdate);
     }
 }
--- src/test/java/fr/houssam/kata/account/business/AccountServiceTest.java
+++ src/test/java/fr/houssam/kata/account/business/AccountServiceTest.java
@@ -1,10 +1,13 @@
 package fr.houssam.kata.account.business;
 
+import fr.houssam.kata.account.domain.Account;
+import fr.houssam.kata.account.domain.Customer;
 import fr.houssam.kata.account.repository.AccountRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -23,6 +26,16 @@ public class AccountServiceTest {
 
     @Test
     public void should_make_a_deposit_in_a_given_account() {
+        Mockito.doReturn(Account.builder()
+                .id(1L)
+                .numero("10025135")
+                .customer(new Customer())
+                .solde(1000L)
+                .build()).when(accountRepository).findByNumero("10025135");
+
         accountService.depose(100L, "10025135");
+
+        Mockito.verify(accountRepository, Mockito.times(1)).findByNumero("10025135");
+        Mockito.verify(accountRepository, Mockito.times(1)).save(Mockito.any(Account.class));
     }
 }
