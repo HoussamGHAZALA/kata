03e78bed41a2e27c246efd4c4fb78198fcd8fd3d
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-09 10:25:55 +0100

    User can make withdrawl on his account.

--- src/main/java/fr/houssam/kata/account/api/AccountController.java
+++ src/main/java/fr/houssam/kata/account/api/AccountController.java
@@ -3,6 +3,7 @@ package fr.houssam.kata.account.api;
 import fr.houssam.kata.account.business.AccountService;
 import fr.houssam.kata.account.domain.Account;
 import fr.houssam.kata.account.domain.Amount;
+import fr.houssam.kata.account.domain.OperationType;
 import fr.houssam.kata.exception.AccountNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +22,13 @@ public class AccountController {
     }
 
     @RequestMapping(method = RequestMethod.PUT, path = "/api/accounts/{accountNumero}/operations/{operationType}")
-    public Account update(@PathVariable String accountNumero, @PathVariable String operationType, @RequestBody Amount amount) {
+    public Account update(@PathVariable String accountNumero, @PathVariable OperationType operationType, @RequestBody Amount amount) {
         Account account = accountService.fetchByNumero(accountNumero)
                 .orElseThrow(() -> new AccountNotFoundException("Account not found, please check your account numero"));
-
-        return accountService.depose(amount, account);
+        switch (operationType) {
+            case  DEPOSIT: return accountService.depose(amount, account);
+            case  WITHDRAWL: return accountService.withdraw(amount, account);
+        }
+        throw new IllegalArgumentException("Le type d'opération donné ne correspondant à aucun type.");
     }
 }
