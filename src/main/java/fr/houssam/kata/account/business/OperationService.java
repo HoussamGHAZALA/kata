c7afbd933b028eec8a084ec68b18c430bd6c0c5d
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-09 11:47:01 +0100

    Init History service.

---
+++ src/main/java/fr/houssam/kata/account/business/OperationService.java
@@ -0,0 +1,7 @@
+package fr.houssam.kata.account.business;
+
+/**
+ * Created by ghazala on 09/12/16.
+ */
+public class OperationService {
+}
---
+++ src/test/java/fr/houssam/kata/account/business/OperationServiceTest.java
@@ -0,0 +1,17 @@
+package fr.houssam.kata.account.business;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by ghazala on 09/12/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OperationServiceTest {
+
+    @Test
+    public void init() throws Exception {
+
+    }
+}
