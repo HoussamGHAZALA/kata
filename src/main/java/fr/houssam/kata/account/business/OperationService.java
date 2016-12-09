1d95fc03e4435517cf5e86a43a4c27a3afa3955d
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-09 11:56:38 +0100

    History of account service done.

--- src/main/java/fr/houssam/kata/account/business/OperationService.java
+++ src/main/java/fr/houssam/kata/account/business/OperationService.java
@@ -1,7 +1,23 @@
 package fr.houssam.kata.account.business;
 
+import fr.houssam.kata.account.domain.Account;
+import fr.houssam.kata.account.domain.Operation;
+import fr.houssam.kata.account.repository.OperationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by ghazala on 09/12/16.
  */
+@Service
 public class OperationService {
+
+    @Autowired
+    private OperationRepository operationRepository;
+
+    public List<Operation> fetchBy(Account account) {
+        return operationRepository.findByAccount(account);
+    }
 }
--- src/test/java/fr/houssam/kata/account/business/OperationServiceTest.java
+++ src/test/java/fr/houssam/kata/account/business/OperationServiceTest.java
@@ -1,8 +1,24 @@
 package fr.houssam.kata.account.business;
 
+import fr.houssam.kata.account.domain.Account;
+import fr.houssam.kata.account.domain.Customer;
+import fr.houssam.kata.account.domain.Operation;
+import fr.houssam.kata.account.repository.OperationRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static fr.houssam.kata.account.domain.OperationType.DEPOSIT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 
 /**
  * Created by ghazala on 09/12/16.
@@ -10,8 +26,50 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class OperationServiceTest {
 
-    @Test
-    public void init() throws Exception {
+    @Mock
+    private OperationRepository operationRepository;
 
+    @Spy
+    @InjectMocks
+    private OperationService operationService;
+
+    private Customer customer;
+    private Operation depot;
+    private Operation retrait;
+    private Account account;
+
+    @Before
+    public void init() {
+        customer = Customer.builder().nom("TOTO").prenom("TATA").build();
+        account = Account.builder()
+                .numero("11005BC589")
+                .solde(500L)
+                .customer(customer)
+                .build();
+        depot = Operation.builder()
+                .amount(50L)
+                .date(new Date(1111L))
+                .account(account)
+                .operationType(DEPOSIT).build();
+        retrait = Operation.builder()
+                .amount(300L)
+                .date(new Date(22222L))
+                .account(account)
+                .operationType(DEPOSIT).build();
+    }
+
+    @Test
+    public void should_fetch_all_operations_by_account() throws Exception {
+        List<Operation> opes = new ArrayList<Operation>();
+        opes.add(depot);
+        opes.add(retrait);
+        doReturn(opes).when(operationRepository).findByAccount(account);
+
+        List<Operation> operations = operationService.fetchBy(account);
+
+        assertThat(operations).isNotNull()
+                .isNotEmpty()
+                .hasSize(2)
+                .containsExactly(depot, retrait);
     }
 }
